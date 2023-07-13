bd58e4495130ee67befa4c2a7343f6311146e081
KyLenMou <2729269812@qq.com>
2023-07-13 14:13:46 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 2\Codeforces Round 884 (Div. 1 + Div. 2)\A.cpp

---
+++ Div. 2/Codeforces Round 884 (Div. 1 + Div. 2)/A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define IOS ios::sync_with_stdio(false), cin.tie(0), cout.tie(0) 
+#define endl '\n'
+#define ll long long
+#define pq(x) priority_queue<x,vector<x>,greater<x>>
+#define PQ(x) priority_queue<x>
+using namespace std;
+typedef pair<int,int> PII;
+
+const int N = 0;
+
+void solve(){
+    int  a,b;
+    cin >> a >> b;
+    cout << a + b << endl; 
+}
+
+int main()
+{
+	IOS;
+	int T;
+	cin >> T;
+	while(T--) solve();
+	return 0;
+} 
