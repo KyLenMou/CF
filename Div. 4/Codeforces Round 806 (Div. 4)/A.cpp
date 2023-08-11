272911a8d2e0e00ba080577ad3bd1b128c1a1e15
KyLenMou <2729269812@qq.com>
2023-08-11 16:03:19 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 4\Codeforces Round 806 (Div. 4)\A.cpp

---
+++ Div. 4/Codeforces Round 806 (Div. 4)/A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define IOS ios::sync_with_stdio(false), cin.tie(0), cout.tie(0) 
+#define endl '\n'
+// #define int long long
+#define ll long long
+#define pq(x) priority_queue<x,vector<x>,greater<x>>
+#define PQ(x) priority_queue<x>
+using namespace std;
+typedef pair<int,int> PII;
+
+const int N = 0;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; s[i]; i++) s[i] = toupper(s[i]);
+    if (s == "YES") cout << "YES" << endl;
+    else cout << "NO" << endl; 
+}
+
+int32_t main()
+{
+	IOS;
+	int T;
+	cin >> T;
+	//T = 1;
+	while(T--) solve();
+	return 0;
+} 
