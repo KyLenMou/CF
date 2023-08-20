7346b952b06ede62a987c313b21cceb3e2aba896
KyLenMou <2729269812@qq.com>
2023-08-20 11:54:32 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Educational Codeforces Round\Educational Codeforces Round 153\A.cpp

---
+++ Educational Codeforces Round/Educational Codeforces Round 153/A.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define IOS ios::sync_with_stdio(false), cin.tie(0), cout.tie(0) 
+#define endl '\n'
+// #define int long long
+#define ll long long
+#define pq(x) priority_queue<x,vector<x>,greater<x>>
+#define PQ(x) priority_queue<x>
+using namespace std;
+typedef pair<int,int> PII;
+
+const int N = 0;    
+
+void solve()
+{   
+    string s;
+    cin >> s;
+    int n = s.size();
+    int cnt = 0;
+    if (s == "()") cout << "NO" << endl;
+    else
+    {
+        cout << "YES" << endl; 
+        if (s.find("))") == s.npos && s.find("((") == s.npos) cout << string(n,'(') + string(n,')') << endl;
+        else 
+        {
+            while (n--) 
+            {
+                cout << "()";
+            }
+            cout << endl;
+        }
+    }
+    
+}
+
+int32_t main()
+{
+	IOS;
+	int T;
+	cin >> T;
+	//T = 1;
+	while(T--) solve();
+	return 0;
+}
