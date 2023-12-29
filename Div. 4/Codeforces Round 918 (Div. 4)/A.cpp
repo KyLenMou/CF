be642bce2ab5f103c36312b2e4f22eae616ab0c2
KyLenMou <2729269812@qq.com>
2023-12-29 12:11:23 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 4\Codeforces Round 918 (Div. 4)\A.cpp

---
+++ Div. 4/Codeforces Round 918 (Div. 4)/A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define endl '\n'
+// #define int long long
+#define ll long long
+#define pq(x) priority_queue<x,vector<x>,greater<x>>
+#define PQ(x) priority_queue<x>
+using namespace std;
+typedef pair<int,int> PII;
+
+void solve() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    if (a == b) cout << c << endl;
+    else if (a == c) cout << b << endl;
+    else cout << a << endl;
+}
+
+int32_t main() {
+	ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	int T;
+	cin >> T;
+	while(T--) solve();
+	return 0;
+}
