8545c983b791d266f9557edbd4d001151bfe38ca
KyLenMou <2729269812@qq.com>
2024-01-17 16:08:40 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 2\Codeforces Round 919 (Div. 2)\D.cpp

---
+++ Div. 2/Codeforces Round 919 (Div. 2)/D.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define RainRingBell int32_t main()
+#define KyLen ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define Kshqsz int T;cin >> T;while(T--) solve()
+#define Kitto return 0
+#define x first
+#define y second
+#define endl '\n'
+#define pq(x) priority_queue<x,vector<x>,greater<x>>
+#define PQ(x) priority_queue<x>
+#define int long long
+using namespace std;
+
+void solve() {
+
+}
+
+RainRingBell {
+    KyLen;
+    Kshqsz;
+    Kitto;
+}
