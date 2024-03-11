960cc2733c26cb7a29f5c8d737e1b09ff69b231b
KyLenMou <2729269812@qq.com>
2024-03-11 23:12:06 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 3\Codeforces Round 933 (Div. 3)\C.cpp

---
+++ Div. 3/Codeforces Round 933 (Div. 3)/C.cpp
@@ -0,0 +1,46 @@
+/* \/\ \         /\ \                        
+\ \ \/'/'  __  __\ \ \         __    ___     
+ \ \ , <  /\ \/\ \\ \ \  __  /'__`\/' _ `\   
+  \ \ \\`\\ \ \_\ \\ \ \L\ \/\  __//\ \/\ \  
+   \ \_\ \_\/`____ \\ \____/\ \____\ \_\ \_\ 
+    \/_/\/_/`/___/> \\/___/  \/____/\/_/\/_/ 
+               /\___/                        
+       ___ ___ \/__/      qq: 2729269812     
+     /' __` __`\       email: kkkylen@qq.com 
+     /\ \/\ \/\ \    ___   __  __            
+     \ \ \ \ \ \ \  / __`\/\ \/\ \           
+      \ \ \ \ \ \ \/\ \L\ \ \ \_\ \          
+       \ \ \ \ \ \ \ \____/\ \____/          
+        \/_/\/_/\/_/\/___/  \/__*/           
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define endl '\n'
+#define int long long
+using namespace std;
+
+void KyLen() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    s = '?' + s;
+    int num = 0;
+    for (int i = 1; i <= n - 2; i++) {
+        if (s.substr(i,3) == "pie" || s.substr(i,3) == "map") {
+            num++;
+        }
+    }
+    for (int i = 1; i <= n - 4; i++) {
+        if (s.substr(i,5) == "mapie") {
+            num--;
+        }
+    }
+    cout << num << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T; cin >> T; while(T--) KyLen();
+    return 0;
+}
