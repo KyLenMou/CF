1d3f86bf7afedb6cd0bccb4d979283cd87833dbf
KyLenMou <2729269812@qq.com>
2024-03-20 00:23:07 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 3\Codeforces Round 935 (Div. 3)\A.cpp

---
+++ Div. 3/Codeforces Round 935 (Div. 3)/A.cpp
@@ -0,0 +1,36 @@
+/* \/\ \         /\ \                        
+\ \ \/'/'  __  __\ \ \         __    ___     
+ \ \ , <  /\ \/\ \\ \ \  __  /'__`\/' _ `\   
+  \ \ \\`\\ \ \_\ \\ \ \L\ \/\  __//\ \/\ \  
+   \ \_\ \_\/`____ \\ \____/\ \____\ \_\ \_\ 
+    \/_/\/_/`/___/> \\/___/  \/____/\/_/\/_/ 
+               /\___/                        
+       ___ ___ \/__/      qq: 2729269812     
+     /' __` __`\       email: kkkylen@qq.com 
+     /\ \/\ \/\ \    ___   __  __            
+     \ \ \ \ \ \ \  / __`\/\ \/\ \           
+      \ \ \ \ \ \ \/\ \L\ \ \ \_\ \          
+       \ \ \ \ \ \ \ \____/\ \____/          
+        \/_/\/_/\/_/\/___/  \/__*/           
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define endl '\n'
+#define int long long
+using namespace std;
+
+void KyLen() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    if (b % 3 > 0 && b % 3 + c < 3) {
+        cout << -1 << endl;
+        return;
+    }
+    cout << a + b / 3 + (b % 3 + c + 2) / 3 << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T; cin >> T; while(T--) KyLen();
+    return 0;
+}
