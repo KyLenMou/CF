61825c6b5567adcb56c6a36993992b0ef94aef0c
KyLenMou <2729269812@qq.com>
2024-03-28 23:18:46 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 4\Codeforces Round 937 (Div. 4)\D.cpp

---
+++ Div. 4/Codeforces Round 937 (Div. 4)/D.cpp
@@ -0,0 +1,51 @@
+/* \/\ \         /\ \                        
+\ \ \/'/'  __  __\ \ \         __    ___     
+ \ \ , <  /\ \/\ \\ \ \  __  /'__`\/' _ `\   
+  \ \ \\`\\ \ \_\ \\ \ \L\ \/\  __//\ \/\ \  
+   \ \_\ \_\/`____ \\ \____/\ \____\ \_\ \_\ 
+    \/_/\/_/`/___/> \\/___/  \/____/\/_/\/_/ 
+               /\___/                        
+       ___ ___ \/__/      qq: 2729269812     
+     /' __` __`\       email: kkkylen@qq.com 
+     /\ \/\ \/\ \    ___   __  __            
+     \ \ \ \ \ \ \  / __`\/\ \/\ \           
+      \ \ \ \ \ \ \/\ \L\ \ \ \_\ \          
+       \ \ \ \ \ \ \ \____/\ \____/          
+        \/_/\/_/\/_/\/___/  \/__*/           
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define endl '\n'
+#define int long long
+using namespace std;
+
+int g[] = {10, 11, 100, 101, 110, 111, 1000, 1001, 1010, 1011, 1100, 1101, 1110, 1111, 10000, 10001, 10010, 10011, 10100, 10101, 10110, 10111, 11000, 11001, 11010, 11011, 11100, 11101, 11110, 11111, 100000};
+int f(int x) {
+    for (int i = 29; i >= 0; i--) if (x % g[i] == 0) return x / g[i];
+    return 0;
+}
+
+void KyLen() {
+    int n;
+    cin >> n;
+    if (n == 1) {
+        cout << "YES" << endl;
+        return;
+    }
+    while (1) {
+        if (f(n)) {
+            n = f(n);
+            if (n == 1) {
+                cout << "YES" << endl;
+                return;
+            }
+        } else break;
+    }
+    cout << "NO" << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T; cin >> T; while(T--) KyLen();
+    return 0;
+}
