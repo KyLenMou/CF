6f89026acb3afe0a913d3c22f0ab412107f00e0c
KyLenMou <2729269812@qq.com>
2024-05-01 17:33:19 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 3\Codeforces Round 913 (Div. 3)\C.cpp

---
+++ Div. 3/Codeforces Round 913 (Div. 3)/C.cpp
@@ -0,0 +1,43 @@
+/* \/\ \         /\ \                        
+\ \ \/'/'  __  __\ \ \         __    ___     
+ \ \ , <  /\ \/\ \\ \ \  __  /'__`\/' _ `\   
+  \ \ \\`\\ \ \_\ \\ \ \L\ \/\  __//\ \/\ \  
+   \ \_\ \_\/`____ \\ \____/\ \____\ \_\ \_\ 
+    \/_/\/_/`/___/> \\/___/  \/____/\/_/\/_/ 
+               /\___/                        
+       ___ ___ \/__/      qq: 2729269812     
+     /' __` __`\       email: kkkylen@qq.com 
+     /\ \/\ \/\ \    ___   __  __            
+     \ \ \ \ \ \ \  / __`\/\ \/\ \           
+      \ \ \ \ \ \ \/\ \L\ \ \ \_\ \          
+       \ \ \ \ \ \ \ \____/\ \____/          
+        \/_/\/_/\/_/\/___/  \/__*/           
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define endl '\n'
+#define int long long
+using namespace std;
+
+void KyLen() {
+    int n;
+    string s;
+    cin >> n >> s;
+    s = "?" + s;
+    int num = 1;
+    map<char,int> m;
+    for (int i = 1; i <= n; i++) {
+        m[s[i]]++;
+        num = max(num,m[s[i]]);
+    }
+    if (num <= (n + 1) / 2) cout << (n & 1) << endl;
+    else {
+        cout << num - n + num << endl;
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T; cin >> T; while(T--) KyLen();
+    return 0;
+}
