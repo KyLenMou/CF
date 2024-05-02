a8f3ff666cb73903afaab131d6e317296f4a2700
KyLenMou <2729269812@qq.com>
2024-05-02 22:54:25 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 3\Codeforces Round 943 (Div. 3)\B.cpp

---
+++ Div. 3/Codeforces Round 943 (Div. 3)/B.cpp
@@ -0,0 +1,45 @@
+/* \/\ \         /\ \                        
+\ \ \/'/'  __  __\ \ \         __    ___     
+ \ \ , <  /\ \/\ \\ \ \  __  /'__`\/' _ `\   
+  \ \ \\`\\ \ \_\ \\ \ \L\ \/\  __//\ \/\ \  
+   \ \_\ \_\/`____ \\ \____/\ \____\ \_\ \_\ 
+    \/_/\/_/`/___/> \\/___/  \/____/\/_/\/_/ 
+               /\___/                        
+       ___ ___ \/__/      qq: 2729269812     
+     /' __` __`\       email: kkkylen@qq.com 
+     /\ \/\ \/\ \    ___   __  __            
+     \ \ \ \ \ \ \  / __`\/\ \/\ \           
+      \ \ \ \ \ \ \/\ \L\ \ \ \_\ \          
+       \ \ \ \ \ \ \ \____/\ \____/          
+        \/_/\/_/\/_/\/___/  \/__*/           
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define endl '\n'
+#define int long long
+using namespace std;
+
+void KyLen() {
+    int n,m;
+    cin >> n >> m;
+    string a,b;
+    cin >> a >> b;
+    a = "?" + a;
+    b = "?" + b;
+    int p = 1, q = 1, cnt = 0;
+    while (p <= n && q <= m) {
+        if (a[p] == b[q]) {
+            p++, q++;
+            cnt++;
+        } else {
+            q++;
+        }
+    }
+    cout << cnt << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T; cin >> T; while(T--) KyLen();
+    return 0;
+}
