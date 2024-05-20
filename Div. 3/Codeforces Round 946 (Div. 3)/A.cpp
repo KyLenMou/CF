7b200cdf5414c1ee5a3e76ba8eee53b7bf89013f
KyLenMou <2729269812@qq.com>
2024-05-20 22:43:37 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 3\Codeforces Round 946 (Div. 3)\A.cpp

---
+++ Div. 3/Codeforces Round 946 (Div. 3)/A.cpp
@@ -0,0 +1,40 @@
+/* \/\ \         /\ \                        
+\ \ \/'/'  __  __\ \ \         __    ___     
+ \ \ , <  /\ \/\ \\ \ \  __  /'__`\/' _ `\   
+  \ \ \\`\\ \ \_\ \\ \ \L\ \/\  __//\ \/\ \  
+   \ \_\ \_\/`____ \\ \____/\ \____\ \_\ \_\ 
+    \/_/\/_/`/___/> \\/___/  \/____/\/_/\/_/ 
+               /\___/                        
+       ___ ___ \/__/      qq: 2729269812     
+     /' __` __`\       email: kkkylen@qq.com 
+     /\ \/\ \/\ \    ___   __  __            
+     \ \ \ \ \ \ \  / __`\/\ \/\ \           
+      \ \ \ \ \ \ \/\ \L\ \ \ \_\ \          
+       \ \ \ \ \ \ \ \____/\ \____/          
+        \/_/\/_/\/_/\/___/  \/__*/           
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define endl '\n'
+#define int long long
+using namespace std;
+
+void KyLen() {
+    int x,y;
+    cin >> x >> y;
+    if (y & 1) {
+        int num = y / 2 + 1;
+        if (x <= (num - 1) * 7 + 11) cout << num << endl;
+        else cout << num + (x - ((num - 1) * 7 + 11) + 14) / 15 << endl;
+    } else {
+        int num = y / 2;
+        if (x <= num * 7) cout << num << endl;
+        else cout << num + (x - num * 7 + 14) / 15 << endl;
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T; cin >> T; while(T--) KyLen();
+    return 0;
+}
