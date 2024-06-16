d0dbf0bc4e9d8b26c1aa7bf722f4c0550de8e80a
KyLenMou <2729269812@qq.com>
2024-06-16 17:44:15 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 2\Codeforces Round 953 (Div. 2)\B.cpp

---
+++ Div. 2/Codeforces Round 953 (Div. 2)/B.cpp
@@ -0,0 +1,45 @@
+/* \/\ \         /\ \                        
+\ \ \/'/'  __  __\ \ \         __    ___     
+ \ \ , <  /\ \/\ \\ \ \  __  /'__`\/' _ `\   
+  \ \ \\`\\ \ \_\ \\ \ \L\ \/\  __//\ \/\ \  
+   \ \_\ \_\/`____ \\ \____/\ \____\ \_\ \_\ 
+    \/_/\/_/`/___/> \\/___/  \/____/\/_/\/_/ 
+               /\___/                        
+       ___ ___ \/__/      qq: 2729269812     
+     /' __` __`\       email: kkkylen@qq.com 
+     /\ \/\ \/\ \    ___   __  __            
+     \ \ \ \ \ \ \  / __`\/\ \/\ \           
+      \ \ \ \ \ \ \/\ \L\ \ \ \_\ \          
+       \ \ \ \ \ \ \ \____/\ \____/          
+        \/_/\/_/\/_/\/___/  \/__*/           
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define endl '\n'
+#define int long long
+using namespace std;
+
+void KyLen() {
+    int n,a,b;
+    cin >> n >> a >> b;
+    int ans = 0;
+    if (a >= b) {
+        cout << n * a << endl;
+        return;
+    }
+    int k = min(n, b - a - 1);
+    ans = max(ans, k * (b + 1) - k * (k + 1) / 2 + a * (n - k));
+    k++;
+    if (k > n) {
+        cout << ans << endl;
+        return;
+    }
+    ans = max(ans, k * (b + 1) - k * (k + 1) / 2 + a * (n - k));
+    cout << ans << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T; cin >> T; while(T--) KyLen();
+    return 0;
+}
