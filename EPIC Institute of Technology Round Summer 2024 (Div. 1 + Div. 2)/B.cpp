12fb835ae2246c16151e1e69cc40af67c63f8fb9
KyLenMou <2729269812@qq.com>
2024-06-30 22:46:35 +0800

    commit E:\Project\AlgorithmCode\CodeForces\EPIC Institute of Technology Round Summer 2024 (Div. 1 + Div. 2)\B.cpp

---
+++ EPIC Institute of Technology Round Summer 2024 (Div. 1 + Div. 2)/B.cpp
@@ -0,0 +1,43 @@
+/* \/\ \         /\ \                        
+\ \ \/'/'  __  __\ \ \         __    ___     
+ \ \ , <  /\ \/\ \\ \ \  __  /'__`\/' _ `\   
+  \ \ \\`\\ \ \_\ \\ \ \L\ \/\  __//\ \/\ \  
+   \ \_\ \_\/`____ \\ \____/\ \____\ \_\ \_\ 
+    \/_/\/_/`/___/> \\/___/  \/____/\/_/\/_/ 
+               /\___/                        
+       ___ ___ \/__/      qq: 2729269812     
+     /' __` __`\       email: kkkylen@qq.com 
+     /\ \/\ \/\ \    ___   __  __            
+     \ \ \ \ \ \ \  / __`\/\ \/\ \           
+      \ \ \ \ \ \ \/\ \L\ \ \ \_\ \          
+       \ \ \ \ \ \ \ \____/\ \____/          
+        \/_/\/_/\/_/\/___/  \/__*/           
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define endl '\n'
+#define int long long
+using namespace std;
+
+void KyLen() {
+    int n;
+    cin >> n;
+    stack<int> stk;
+    int ans = 0;
+    int maxa = 0;
+    for (int i = 1; i <= n; i++) {
+        int a;
+        cin >> a;
+        if (stk.size() && stk.top() > a) {
+            maxa = max(maxa, stk.top() - a);
+            ans += stk.top() - a;
+        } else stk.push(a);
+    }
+    cout << maxa + ans << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T; cin >> T; while(T--) KyLen();
+    return 0;
+}
