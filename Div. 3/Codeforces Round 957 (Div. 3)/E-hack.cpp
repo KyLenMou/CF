f6f9d5292ef55c86b0e109c84edb24fd98c78f93
KyLenMou <2729269812@qq.com>
2024-07-12 01:35:54 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 3\Codeforces Round 957 (Div. 3)\E-hack.cpp

---
+++ Div. 3/Codeforces Round 957 (Div. 3)/E-hack.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define RainRingBell int32_t main()
+#define KyLen ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define Kshqsz solve()
+#define Kitto return 0
+#define x first
+#define y second
+#define endl '\n'
+#define pq(x) priority_queue<x,vector<x>,greater<x>>
+#define PQ(x) priority_queue<x>
+#define int long long
+using namespace std;
+
+void solve() {
+    cout << 100 << endl;
+    for (int i = 1; i <= 99; i++) {
+        cout << 100 << endl;
+    }
+    cout << 100;
+}
+
+RainRingBell {
+    KyLen;
+    Kshqsz;
+    Kitto;
+}
