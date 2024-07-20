24f92211db79147b90f00711dc0d46a440d69f9d
KyLenMou <2729269812@qq.com>
2024-07-20 21:39:48 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 2\Codeforces Round 959 sponsored by NEAR (Div. 1 + Div. 2)\B.cpp

---
+++ Div. 2/Codeforces Round 959 sponsored by NEAR (Div. 1 + Div. 2)/B.cpp
@@ -0,0 +1,92 @@
+/* \/\ \         /\ \                        
+\ \ \/'/'  __  __\ \ \         __    ___     
+ \ \ , <  /\ \/\ \\ \ \  __  /'__`\/' _ `\   
+  \ \ \\`\\ \ \_\ \\ \ \L\ \/\  __//\ \/\ \  
+   \ \_\ \_\/`____ \\ \____/\ \____\ \_\ \_\ 
+    \/_/\/_/`/___/> \\/___/  \/____/\/_/\/_/ 
+               /\___/                        
+       ___ ___ \/__/      qq: 2729269812     
+     /' __` __`\       email: kkkylen@qq.com 
+     /\ \/\ \/\ \    ___   __  __            
+     \ \ \ \ \ \ \  / __`\/\ \/\ \           
+      \ \ \ \ \ \ \/\ \L\ \ \ \_\ \          
+       \ \ \ \ \ \ \ \____/\ \____/          
+        \/_/\/_/\/_/\/___/  \/__*/           
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define endl '\n'
+#define int long long
+using namespace std;
+
+void KyLen() {
+    int n;
+    cin >> n;
+    string a,b;
+    cin >> a >> b;
+    for (int i = 0; a[i]; i++) {
+        if (a[i] == '0') {
+            if (b[i] == '1') {
+                cout << "NO" << endl;
+                return;
+            }
+        } else {
+            break;
+        }
+    }
+    cout << "YES" << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T; cin >> T; while(T--) KyLen();
+    return 0;
+}
+// /* \/\ \         /\ \                        
+// \ \ \/'/'  __  __\ \ \         __    ___     
+//  \ \ , <  /\ \/\ \\ \ \  __  /'__`\/' _ `\   
+//   \ \ \\`\\ \ \_\ \\ \ \L\ \/\  __//\ \/\ \  
+//    \ \_\ \_\/`____ \\ \____/\ \____\ \_\ \_\ 
+//     \/_/\/_/`/___/> \\/___/  \/____/\/_/\/_/ 
+//                /\___/                        
+//        ___ ___ \/__/      qq: 2729269812     
+//      /' __` __`\       email: kkkylen@qq.com 
+//      /\ \/\ \/\ \    ___   __  __            
+//      \ \ \ \ \ \ \  / __`\/\ \/\ \           
+//       \ \ \ \ \ \ \/\ \L\ \ \ \_\ \          
+//        \ \ \ \ \ \ \ \____/\ \____/          
+//         \/_/\/_/\/_/\/___/  \/__*/           
+// #include<bits/stdc++.h>
+// #define x first
+// #define y second
+// #define endl '\n'
+// #define int long long
+// using namespace std;
+
+// void KyLen() {
+//     int n;
+//     cin >> n;
+//     string a,b;
+//     cin >> a >> b;
+//     if (a == b || a[0] == '1') {
+//         cout << "YES" << endl;
+//         return;
+//     }
+//     for (int i = 0; a[i]; i++) {
+//         if (a[i] == '0') {
+//             if (b[i] == '1') {
+//                 cout << "NO" << endl;
+//                 return;
+//             }
+//         } else {
+//             break;
+//         }
+//     }
+//     cout << "YES" << endl;
+// }
+
+// int32_t main() {
+//     ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+//     int T; cin >> T; while(T--) KyLen();
+//     return 0;
+// }
