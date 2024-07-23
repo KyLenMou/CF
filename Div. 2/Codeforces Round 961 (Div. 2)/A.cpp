a2a3de744e28bedee63af0c899bb6eb96c0ad1b5
KyLenMou <2729269812@qq.com>
2024-07-23 22:43:44 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 2\Codeforces Round 961 (Div. 2)\A.cpp

---
+++ Div. 2/Codeforces Round 961 (Div. 2)/A.cpp
@@ -0,0 +1,49 @@
+/* \/\ \         /\ \                        
+\ \ \/'/'  __  __\ \ \         __    ___     
+ \ \ , <  /\ \/\ \\ \ \  __  /'__`\/' _ `\   
+  \ \ \\`\\ \ \_\ \\ \ \L\ \/\  __//\ \/\ \  
+   \ \_\ \_\/`____ \\ \____/\ \____\ \_\ \_\ 
+    \/_/\/_/`/___/> \\/___/  \/____/\/_/\/_/ 
+               /\___/                        
+       ___ ___ \/__/      qq: 2729269812     
+     /' __` __`\       email: kkkylen@qq.com 
+     /\ \/\ \/\ \    ___   __  __            
+     \ \ \ \ \ \ \  / __`\/\ \/\ \           
+      \ \ \ \ \ \ \/\ \L\ \ \ \_\ \          
+       \ \ \ \ \ \ \ \____/\ \____/          
+        \/_/\/_/\/_/\/___/  \/__*/           
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define endl '\n'
+#define int long long
+using namespace std;
+
+void KyLen() {
+    int n,k;
+    cin >> n >> k;
+    int ans = 0;
+    if (k >= n) {
+        k -= n;
+        ans++;
+    }
+    for (int i = n - 1; i >= 1; i--) {
+        if (k >= i) {
+            k -= i;
+            ans++;
+        }
+        if (k >= i) {
+            k -= i;
+            ans++;
+        }
+        if (k < i) break;
+    }
+    if (k) ans++;
+    cout << ans << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T; cin >> T; while(T--) KyLen();
+    return 0;
+}
