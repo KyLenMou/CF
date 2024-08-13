aae344dc95529c97aceda75448946d667ada33d2
KyLenMou <2729269812@qq.com>
2024-08-13 22:45:10 +0800

    commit E:\Project\AlgorithmCode\CodeForces\Div. 3\Codeforces Round 966 (Div. 3)\A.cpp

---
+++ Div. 3/Codeforces Round 966 (Div. 3)/A.cpp
@@ -0,0 +1,45 @@
+/* \/\ \         /\ \                        
+\ \ \/'/'  __  __\ \ \         __    ___     
+ \ \ , <  /\ \/\ \\ \ \  __  /'__`\/' _ `\   
+  \ \ \\`\\ \ \_\ \\ \ \L\ \/\  __//\ \/\ \  
+   \ \_\ \_\/`____ \\ \____/\ \____\ \_\ \_\ 
+    \/_/\/_/`/___/> \\/___/  \/____/\/_/\/_/ 
+               /\___/                        
+       ___ ___ \/__/      qq: 2729269812     
+     /' __` __`\       email: kkkylen@qq.com 
+     /\ \/\ \/\ \    ___   __  __            
+     \ \ \ \ \ \ \  / __`\/\ \/\ \           
+      \ \ \ \ \ \ \/\ \L\ \ \ \_\ \          
+       \ \ \ \ \ \ \ \____/\ \____/          
+        \/_/\/_/\/_/\/___/  \/__*/           
+#include<bits/stdc++.h>
+#define x first
+#define y second
+#define endl '\n'
+#define int long long
+using namespace std;
+
+void KyLen() {
+    string s;
+    cin >> s;
+    if (s.size() < 3) cout << "NO" << endl;
+    else {
+        if (s[0] == '1' && s[1] == '0') {
+            if (s.size() == 3){
+                if (s[2] == '1' || s[2] == '0') cout << "NO" << endl;
+                else cout << "YES" << endl; 
+            } else {
+                if (s[2] == '0') cout << "NO" << endl;
+                else cout << "YES" << endl;
+            }
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int T; cin >> T; while(T--) KyLen();
+    return 0;
+}
